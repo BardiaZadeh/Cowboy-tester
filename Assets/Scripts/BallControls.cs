3e8f618b904de9ef731208fb2441b0f5f3e42dea
Zadeh, Bardia <besmaeilzadeh3@gatech.edu>
2024-03-18 21:54:32 -0400

    Better control system + Fix jump bugs + added macbook trackpad control for shooting

--- Assets/Scripts/BallControls.cs
+++ Assets/Scripts/BallControls.cs
@@ -5,108 +5,119 @@ using UnityEngine.SceneManagement;
 
 public class BallControls : MonoBehaviour
 {
-  [SerializeField] private float moveSpeed = 5f;
-  [SerializeField] public float jumpPower = 5f;
-  [SerializeField] private Rigidbody rb;
-  //[SerializeField] private float projectileSpeed;
-  //[SerializeField] private Projectile projectilePrefab;
-  public GameOverManager gameOverManager;
-  public GameObject prefabToShoot;
-  public int maxAmmo = 5;
-  public int currentAmmo;
-  private bool isGrounded;
-  private AudioSource bullet;
-  
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] public float jumpPower = 5f;
+    [SerializeField] private Rigidbody rb;
+    //[SerializeField] private float projectileSpeed;
+    //[SerializeField] private Projectile projectilePrefab;
+    public GameOverManager gameOverManager;
+    public GameObject prefabToShoot;
+    public int maxAmmo = 5;
+    public int currentAmmo;
+    private bool isGrounded;
+    private AudioSource bullet;
 
-  void Start()
-  {
-    rb = GetComponent<Rigidbody>();
-    bullet = GetComponent<AudioSource>();
-    currentAmmo = maxAmmo;
-  }
-  void Update() {
-    if (Input.GetMouseButtonDown(0) && currentAmmo > 0) {
-       bullet.Play();
-       Shoot();
-    }
-  }
-  void FixedUpdate()
-  {
-
-    float horizontalInput = Input.GetAxisRaw("Horizontal");
-    float verticalInput = Input.GetAxisRaw("Vertical");
-
-    Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
-
-
-    rb.AddForce(movement);
-
-    isGrounded = IsGrounded();
-    Debug.Log(IsGrounded());
-
-    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    void Start()
     {
-      rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        rb = GetComponent<Rigidbody>();
+        bullet = GetComponent<AudioSource>();
+        currentAmmo = maxAmmo;
     }
-  }
 
-  bool IsGrounded()
-  {
-    // Raycast down to check for ground
-    Ray groundRay = new Ray(transform.position, Vector3.down);
-    RaycastHit hit;
-
-    // Adjust the distance based on your ball size
-    float distance = 1.5f;
-
-    if (Physics.Raycast(groundRay, out hit, distance))
+    void Update()
     {
-      return true;
-    }
-    return false;
-  }
-  void OnCollisionEnter(Collision other)
-  {
-    if (other.gameObject.CompareTag("RollingObject"))
-    {
-       gameOverManager.ShowGameOver();
-    }
-    else if (other.gameObject.CompareTag("NextLevelTotem")){
-        SceneManager.LoadScene("LevelTwoScene");
-    }
-    else if (other.gameObject.CompareTag("ThirdLevelTotem")) {
-        SceneManager.LoadScene("LevelThreeScene");
-    }
-    else if (other.gameObject.CompareTag("FourthLevelTotem")) {
-        SceneManager.LoadScene("LevelFourScene");
-    }
-  }
-  private void RespawnCharacter()
-  {
-    transform.position = new Vector3(0, -5.77f, -83f);
-    rb.velocity = Vector3.zero;
-    return;
-  }
-  void Shoot() {
-    currentAmmo--;
-    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    RaycastHit hit;
-    if (Physics.Raycast(ray, out hit)) {
-      Vector3 target = hit.point;
-      Vector3 direction = target - transform.position;
-      GameObject projectile = Instantiate(prefabToShoot, transform.position, Quaternion.identity);
-      projectile.GetComponent<Rigidbody>().AddForce(direction * 10, ForceMode.Impulse);
-      Physics.IgnoreCollision(projectile.GetComponent<Collider>(), GetComponent<Collider>());
-      foreach (Collider c in GetComponentsInChildren<Collider>()) {
-        Physics.IgnoreCollision(projectile.GetComponent<Collider>(), c);
-      }
-    }
-  }
-  void IncreaseAmmo(int amount) {
-    currentAmmo += amount;
-    if (currentAmmo > maxAmmo) {
-      currentAmmo = maxAmmo;
-    }
-  }
+        if ((Input.GetMouseButtonDown(0) || Input.GetButtonDown("Fire1")) && currentAmmo > 0)
+        {
+            if (bullet != null)
+            {
+                bullet.Play();
+            }
+            else
+            {
+                Debug.LogWarning("AudioSource (bullet) not found.");
+            }
+            Shoot();
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
+        rb.AddForce(movement);
+
+        // Instead of calling IsGrounded() every frame in FixedUpdate, we'll rely on collision detection
+    }
+
+    bool IsGrounded()
+    {
+      // Raycast down to check for ground
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+
+        // Adjust the distance based on your ball size
+        float distance = 1.5f;
+
+        if (Physics.Raycast(groundRay, out hit, distance))
+        {
+            return hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground");
+        }
+        return false;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //  "RollingObject" should be correctly tagged on enemy objects.
+        if (other.gameObject.CompareTag("RollingObject"))
+        {
+            gameOverManager.ShowGameOver();
+        }
+        // Enable jumping again when colliding with the ground
+        if (other.contacts[0].normal.y > 0.5)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Shoot()
+    {
+        if (currentAmmo <= 0) return;
+
+        currentAmmo--;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 direction = hit.point - transform.position;
+            direction.y = 0; // Ensures that the projectile travels horizontally
+            GameObject projectile = Instantiate(prefabToShoot, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+            projectile.GetComponent<Rigidbody>().AddForce(direction.normalized * 1000); // Increase force for faster speed
+        }
+    }
+
+    void IncreaseAmmo(int amount)
+    {
+        currentAmmo += amount;
+        if (currentAmmo > maxAmmo)
+        {
+            currentAmmo = maxAmmo;
+        }
+    }
 }
